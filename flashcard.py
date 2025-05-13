cfe0664ac7b76acaf33acaf6d097f6c340c304a0
wssss <sapolis25@gmail.com>
2025-05-13 21:45:22 +0200

    add nginx headers check page

--- flashcard.py
+++ flashcard.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,14 @@ def welcome():
     return render_template(
         "welcome.html"
     )
+
+@app.route("/nginx")
+def nginx():
+    return jsonify({
+        "remote_addr": request.remote_addr,
+        "x_real_ip": request.headers.get("X-Real-IP"),
+        "x_forwarded_for": request.headers.get("X-Forwarded-For"),
+    })
 
 if __name__ == '__main__':
     app.run()
